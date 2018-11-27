08e3f3c90a82a21e5f82f99224558a52a81e605d
Jorge Bazua <jorgebazua@Jorges-MacBook-Pro.local>
2018-11-27 13:03:21 -0700

    Cambios.

--- src/views/TreeView1.vue
+++ src/views/TreeView1.vue
@@ -1,32 +1,36 @@
 <template>
-<div>
+  <div>
     <v-container class="mt-2 h100">
-        <template>
-            <v-layout row wrap>
-                <v-flex xs12 sm6 md6>
-                    <v-treeview v-model="tree" :open="open" :items="items" :active.sync="active" activatable active-class="primary--text" item-key="name" open-on-click>
-                        <template slot="prepend" slot-scope="{ item, open, leaf }">
-                            <v-icon v-if="!item.file">
-                                {{ open ? 'folder_open' : 'folder' }}
-                            </v-icon>
-                            <v-icon v-else>
-                                assignment
-                            </v-icon>
-                        </template>
-                    </v-treeview>
-                </v-flex>
-                <v-flex xs12 sm4 md4>
-                    <v-text-field label="Padre" v-model="active" disabled outline></v-text-field>
-                    <v-text-field label="Agregar Hijo" v-model="NuevoHijo" outline></v-text-field>
-                    <v-btn fab dark large color="light-green" @click="agregarHijo">
-                        <v-icon dark>subdirectory_arrow_left</v-icon>
-                    </v-btn>
-                </v-flex>
-               
-            </v-layout>
-        </template>
+      <template>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md6>
+            <v-treeview
+              :active.sync="active"
+              :items="items"
+              :open.sync="open"
+              activatable
+              active-class="primary--text"
+              class="grey lighten-5"
+              open-on-click
+              transition
+            >
+              <template slot="prepend" slot-scope="{ item, open, leaf }">
+                <v-icon v-if="!item.file">{{ open ? 'folder_open' : 'folder' }}</v-icon>
+                <v-icon v-else>assignment</v-icon>
+              </template>
+            </v-treeview>
+          </v-flex>
+          <v-flex xs12 sm4 md4>
+            <v-text-field label="Padre" v-model="selected" disabled outline></v-text-field>
+            <v-text-field label="Agregar Hijo" v-model="NuevoHijo" outline></v-text-field>
+            <v-btn fab dark large color="light-green" @click="agregarHijo">
+              <v-icon dark>subdirectory_arrow_left</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </template>
     </v-container>
-</div>
+  </div>
 </template>
 
 <script>
@@ -39,91 +43,79 @@ export default {
     tree: [],
     items: [
       {
-        name: "Vista Arbol",
-        file: false,
+        id: 1,
+        name: ".git"
+      },
+      {
+        id: 2,
+        name: "node_modules"
+      },
+      {
+        id: 3,
+        name: "public",
         children: [
           {
-            name: ".git",
-            file: false
-          },
-          {
-            name: "node_modules",
-            file: false
-          },
-          {
-            name: "public",
-            file: false,
+            id: 4,
+            name: "static",
             children: [
               {
-                name: "static",
-                children: [
-                  {
-                    name: "logo.png",
-                    file: true
-                  },
-                  {
-                    name: "logo2.png",
-                    file: false,
-                    children: [
-                      {
-                        name: "otro",
-                        file: true
-                      },
-                      {
-                        name: "otro2",
-                        file: true
-                      }
-                    ]
-                  }
-                ],
-                file: false
-              },
-              {
-                name: "favicon.ico",
-                file: true
-              },
-              {
-                name: "index.html",
-                file: true
+                id: 5,
+                name: "logo.png",
+                file: "png"
               }
             ]
           },
           {
-            name: ".gitignore",
-            file: true
+            id: 6,
+            name: "favicon.ico",
+            file: "png"
           },
           {
-            name: "babel.config.js",
-            file: true
-          },
-          {
-            name: "package.json",
-            file: true
-          },
-          {
-            name: "README.md",
-            file: true
-          },
-          {
-            name: "vue.config.js",
-            file: true
-          },
-          {
-            name: "yarn.lock",
-            file: false
+            id: 7,
+            name: "index.html",
+            file: "html"
           }
         ]
+      },
+      {
+        id: 8,
+        name: ".gitignore",
+        file: "txt"
+      },
+      {
+        id: 9,
+        name: "babel.config.js",
+        file: "js"
+      },
+      {
+        id: 10,
+        name: "package.json",
+        file: "json"
+      },
+      {
+        id: 11,
+        name: "README.md",
+        file: "md"
+      },
+      {
+        id: 12,
+        name: "vue.config.js",
+        file: "js"
+      },
+      {
+        id: 13,
+        name: "yarn.lock",
+        file: "txt"
       }
     ]
   }),
   computed: {
     selected() {
       if (!this.active.length) return undefined;
-
+      console.log(this.active);
       return this.active;
     }
   },
-
   watch: {
     // active: function() {
     //   alert("Change Active");
